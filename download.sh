6118b5cafddf49269d24a5891374904f78fa1c71
abousgamer <114771325+abousgamer@users.noreply.github.com>
2023-02-10 22:50:03 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 # install package.
 sudo apt update -y
-sudo apt install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc libncurses5 unzip python -y
+sudo apt install bc git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc libncurses5 unzip python -y
 # run repo.
 
 mkdir ~/bin
@@ -25,6 +25,7 @@ git clone https://github.com/youssefnoner/android_manifest_samsung_m10lte.git -b
 
 # Sync
 repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j`nproc`
+repo sync -j1 --fail-fast
 
 # Build
 . build/envsetup.sh && lunch carbon_m10lte-eng && mka clean && mka carbon -j`nproc`
