31baa28c8e2c47ef664cbf04de169063aeca2c0c
abousgamer <114771325+abousgamer@users.noreply.github.com>
2023-02-11 17:42:09 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 # install package.
 sudo apt update -y
-sudo apt install bc git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc libncurses5 unzip python -y
+sudo apt install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc libncurses5 unzip python -y
 # run repo.
 
 mkdir ~/bin
@@ -11,13 +11,13 @@ chmod a+x ~/bin/repo
 sudo ln -sf ~/bin/repo /usr/bin/repo
 
 curl -L -o h.deb http://mirrors.kernel.org/ubuntu/pool/main/b/bc/bc_1.07.1-2_amd64.deb
-dpkg -i h.deb
+sudo dpkg -i h.deb
 
 curl -L -o l.deb http://mirrors.kernel.org/ubuntu/pool/main/p/popt/libpopt0_1.16-11_amd64.deb
-dpkg -i l.deb
+sudo dpkg -i l.deb
 
 curl -L -o i.deb http://security.ubuntu.com/ubuntu/pool/main/r/rsync/rsync_3.1.2-2.1ubuntu1.5_amd64.deb
-dpkg -i i.deb
+sudo dpkg -i i.deb
 
 # set configs.
 git config --global user.email "you@example.com"
